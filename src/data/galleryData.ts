a57ffdd0161f64ba7b3fb795dfc048cdcda77e75
FreddyT2252 <freddytt2252@mail.ugm.ac.id>
2026-01-28 20:17:58 +0700

    ok

--- src/data/galleryData.ts
+++ src/data/galleryData.ts
@@ -4,7 +4,7 @@ export const galleryImagess = [
   "https://cdn.pixabay.com/photo/2017/07/31/11/21/people-2557396_1280.jpg",
   
   // 2. Gotong royong / kerja bakti
-  "https://cdn.pixabay.com/photo/2015/07/02/10/22/training-828726_1280.jpg",
+  "https://id.images.search.yahoo.com/images/view;_ylt=AwrKDm8yDHppA.k2alfNQwx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkA2QxMGM2MTA5NjMxZDVjZTJiMGUxNWYzMmZhMWNmN2I0BGdwb3MDMTMEaXQDYmluZw--?back=https%3A%2F%2Fid.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dlogo%2Bugm%26type%3DE211ID885G0%26fr%3Dmcafee%26fr2%3Dpiv-web%26tab%3Dorganic%26ri%3D13&w=1175&h=436&imgurl=1.bp.blogspot.com%2F-SWJ8QksGDyE%2FVyk3yOP6j1I%2FAAAAAAAAALM%2FvwGfGi7K-pQ3Qvauq_8c4meDTNSZgDD9wCLcB%2Fs1600%2FDaftar%252BLengkap%252BJurusan%252BUGM%252BUniversitas%252BGajah%252BMada%252BTerbaru.jpg&rurl=https%3A%2F%2Fwww.daftarjurusan.id%2F2016%2F05%2Fdaftar-lengkap-jurusan-ugm-universitas-gajah-mada-terbaru.html&size=228KB&p=logo+ugm&oid=d10c6109631d5ce2b0e15f32fa1cf7b4&fr2=piv-web&fr=mcafee&tt=Daftar+Lengkap+Jurusan+UGM+Universitas+Gajah+Mada+Terbaru+-+Daftar+Jurusan&b=0&ni=21&no=13&ts=&tab=organic&sigr=mkr8hUoXdhvW&sigb=gtRY44yPxWCH&sigi=zKODRQ.ErodT&sigt=3lNLMRGQYp2X&.crumb=/XFGjYTZRtl&fr=mcafee&fr2=piv-web&type=E211ID885G0",
   
   // 3. Kegiatan volunteer / relawan
   "https://cdn.pixabay.com/photo/2017/02/18/20/10/volunteer-2078135_1280.jpg",
